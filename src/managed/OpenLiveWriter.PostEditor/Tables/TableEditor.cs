fbcccc845ee36fd455de16b6493505f22f8fa1d0
Nick Vella <nick@nxk.io>
2019-06-26 23:07:15 +1000

    tables: TableEditor.InsertTable: Update design-time borders at table creation time. Fixes tables not initially having gridlines.

--- src/managed/OpenLiveWriter.PostEditor/Tables/TableEditor.cs
+++ src/managed/OpenLiveWriter.PostEditor/Tables/TableEditor.cs
@@ -111,13 +111,22 @@ namespace OpenLiveWriter.PostEditor.Tables
                 // insert the table
                 htmlEditor.InsertHtml(tableHtml.ToString(), false);
 
-                // select the first cell for editing if we have an mshtml selection
+                // If we have an MSHTML available
                 if (insertingIntoMshtml)
                 {
+                    // select the first cell for editing if we have an mshtml selection
                     IHTMLElement[] cells = targetMarkupRange.GetElements(ElementFilters.TABLE_CELL_ELEMENT, true);
                     if (cells.Length > 0)
                     {
                         SelectCell(editorContext, cells[0] as IHTMLTableCell);
+                    }
+
+                    // Update design-time borders
+                    IHTMLElement[] tables = targetMarkupRange.GetElements(ElementFilters.TABLE_ELEMENTS, true);
+                    if(tables.Length > 0)
+                    {
+                        var table = tables[0] as IHTMLTable;
+                        TableHelper.UpdateDesignTimeBorders(table);
                     }
                 }
             }
